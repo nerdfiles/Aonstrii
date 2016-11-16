0bb9182ba646d8baece085aacf3b79051a513883
nerdfiles <nerdfiles@gmail.com>
2016-11-15 21:41:43 -0600

    Notes.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**(
+ * @fileOverview ./src/index.js
+ * @datetime
+ * @description
+ * @ira: 6
+ * )
+ */
+
+(function G () {
+  "use strict";
+  return {
+    "__g__": function KC (s) {
+      for (let i = 1; 1 - y / (1 - y)) {
+        _.each(L, function (p, i) {
+          let length = L.length
+          if (IRA(p) && evaluate(p) == s) {
+            // p-value momentum system
+            return i;
+          }
+       });
+      }
+    }
+  };
+}());
