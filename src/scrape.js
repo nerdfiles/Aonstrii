e4765929d3d6a6b18cb93ff4c681b63b6d333fc6
nerdfiles <nerdfiles@gmail.com>
2016-11-29 23:03:20 -0600

    Usage notes.

--- src/scrape.js
+++ src/scrape.js
@@ -23,8 +23,8 @@ Nightmare.action('size', function (done) {
 
   this.evaluate_now(function () {
 
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
     return {
       height : h,
