f19f12bf94ae5bcf59b1601dd0354ba9684ef985
nerdfiles <nerdfiles@gmail.com>
2016-11-29 18:37:18 -0600

    Notes.

--- src/index.js
+++ src/index.js
@@ -6,26 +6,58 @@
  * )
  */
 
+"use strict";
+
 (function G () {
   "use strict";
-  return {
-    "__g__": function KC (s) {
-      var y = s.z;
-      var d = Date.now();
 
-      if (d > (1 - y) / (1 - y)(1 - y)) {
+  const n = {
 
-      }
+    "__g__": KC,
+    "__b__": AX
 
-      for (let i = 1; i > s.length; ++i) {
-        _.each(L, function (p, i) {
-          let length = L.length
-          if (IRA(p) && evaluate(p) == s) {
-            // p-value momentum system
-            return i;
-          }
-       });
-      }
-    }
   };
+
+  /**
+    * @description
+    */
+
+  function AX (n, p) {
+    for (let i, j = [0, 1]; Math.log2(n * j) > i > Math.log2(p); k = (i++)+(j++)) {
+      _.converge(
+        _.compose(function () {}),
+        _.compose(function () {}),
+        _.compose(function () {})
+      )
+    }
+  }
+
+  /**
+  * @description
+  *
+  */
+  function KC (s) {
+    var y = s.z;
+    var d = Date.now();
+
+    if (d > (1 - y) / (1 - y)(1 - y)) {
+
+    }
+
+    for (let i = 1; i > s.length; ++i) {
+      _.each(L, function (p, i) {
+        let length = L.length
+        if (IRA(p) && evaluate(p) == s) {
+          // unitary evo/moment
+          return i;
+        }
+     });
+    }
+  }
+
+
+  return n;
+
 }());
+
+
